298ee5aadff6ed14f0fa6243fa6e61ff07742d30
Antti Pohjola <summeli@summeli.com>
2024-05-06 18:45:13 +0300

    vim cammands are working
    Signed-off-by: Antti Pohjola <summeli@summeli.com>

--- lua/html-entities.lua
+++ lua/html-entities.lua
@@ -1,22 +1,14 @@
 local htmlEntities = require("html-entities.htmlEntities")
 
-print("kissa")
-
 local he = {}
-
-local function with_defaults(options)
-   return {
-      name = options.name or "John Doe"
-   }
-end
 
 -- This function is supposed to be called explicitly by users to configure this
 -- plugin
-function he.setup(options)
+function he.setup()
    -- avoid setting global values outside of this function. Global state
    -- mutations are hard to debug and test, so having them in a single
    -- function/module makes it easier to reason about all possible changes
-   he.options = with_defaults(options)
+   -- he.options = with_defaults(options)
 
    -- do here any startup your plugin needs, like creating commands and
    -- mappings that depend on values passed in options
@@ -25,11 +17,41 @@ function he.setup(options)
 end
 
 function he.encode()
-   print("koira")
+   local buf = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  
+   local encodedBuf = {}
+   local eob = 0;
+   for n,line in pairs(buf) do 
+      --encode
+     if (line ~= nil and line ~= '' and line ~='\n') then
+	local encodedLine = htmlEntities.encode(line)
+	encodedBuf[n] = encodedLine
+      else
+         encodedBuf[n] = ''
+      end
+      eob = n
+   end
+   --update the buffer
+   vim.api.nvim_buf_set_lines(0, 0, eob, false, encodedBuf)
 end
 
 function he.decode()
-   print("papukaija")
+   local buf = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+   local decodedBuf = {}
+   local eob = 0;
+   for n,line in pairs(buf) do
+      --decode
+     if (line ~= nil and line ~= '' and line ~='\n') then
+        local decodedLine = htmlEntities.decode(line)
+        decodedBuf[n] = decodedLine
+      else
+         decodedBuf[n] = ''
+      end
+      eob = n
+   end
+   --update the buffer
+   vim.api.nvim_buf_set_lines(0, 0, eob, false, decodedBuf)
 end
 
 he.options = nil
