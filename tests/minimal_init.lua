00dbf679ee8e2baf56153702f93085449a715b45
Antti Pohjola <summeli@summeli.com>
2024-05-07 22:05:55 +0300

    stylua
    Signed-off-by: Antti Pohjola <summeli@summeli.com>

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,3 +1,3 @@
-vim.opt.runtimepath:append "."
-vim.cmd.runtime { "plugin/plenary.vim", bang = true }
-vim.cmd.runtime { "plugin/html-entities.nvim", bang = true }
+vim.opt.runtimepath:append('.')
+vim.cmd.runtime({ 'plugin/plenary.vim', bang = true })
+vim.cmd.runtime({ 'plugin/html-entities.nvim', bang = true })
